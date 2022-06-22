e0691b1c6876517ba1a936fcf60a7684e18d1241
ilosrim <ilosrim@yahoo.com>
2022-06-23 01:09:12 +0500

    add navbar, react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import { Home } from './pages'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import logo from '../../assets/icon.svg'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import './navbar.css'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
-    <div className='navbar__container'>
-      <div className="navbar__container-lists">
-        <div className="navbar__container-lists_brand">
-          <img src={logo} alt="" width={40} height={40} />
+    <div className='navbar'>
+      <div className="navbar__links">
+        <div className="navbar__links-logo">
+          <Link to="/">
+            <img src={logo} alt="Bilolab" />
+          </Link>
         </div>
+        <div className="navbar__links-container">
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="navbar__menu">
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+          <div className="navbar__menu_container scale-up-center">
+            <div className="navbar__menu_container-links">
+              <Link to="/">Home</Link>
+              <Link to="/news">News</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
